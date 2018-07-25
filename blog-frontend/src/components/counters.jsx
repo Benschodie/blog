e581144ac0aef747788b996c79f2ff0ed98ab827
Benny <benschodie@gmail.com>
2018-07-25 15:22:02 +0200

    adapt counter ids

--- blog-frontend/src/components/counters.jsx
+++ blog-frontend/src/components/counters.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 1, value: 0 },
-      { id: 1, value: 0 },
-      { id: 1, value: 0 }
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
   };
   render() {
