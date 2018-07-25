75eefd06aa2d62cf3356112290691c0806042b61
Benjamin Konopka <benjamin.konopka@icloud.com>
2018-07-25 14:22:42 +0200

    adding sass to project

--- blog-frontend/src/components/landing.js
+++ blog-frontend/src/components/landing.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1>Landing</h1>
+      // inline styling
+      <div style={{ width: '100%', margin: 'auto' }}>
+        {/* adding a grid */}
+        <Grid className="landing-grid">
+          <Cell col={12} />
+          <h1>Hallo</h1>
+        </Grid>
       </div>
     );
   }
